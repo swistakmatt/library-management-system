9f5ad1052bd0ecc417715962f6f6b37d9269c421
Mateusz Świstak <swistakmatt@gmail.com>
2023-01-10 03:18:02 +0100

    Added WIP LibraryContainer

---
+++ src/media/LibraryContainer.ts
@@ -0,0 +1,120 @@
+/* eslint-disable no-empty */
+import { Album } from './Album';
+import { Song, SongMetadata } from './Song';
+import { Ebook, EbookMetadata } from './Ebook';
+import { Movie, MovieMetadata } from './Movie';
+import { Episode, EpisodeMetadata } from './Episode';
+import { User } from '../users/User';
+import { MediaElement } from './MediaElement';
+
+interface MediaMetadata extends MovieMetadata, EpisodeMetadata, SongMetadata, EbookMetadata {}
+
+export class LibraryContainer {
+  private userMediaLimit: number;
+  private userLibraryLimit: number;
+
+  private movies: Movie[];
+  private episodes: Episode[];
+  private songs: Song[];
+  private ebooks: Ebook[];
+
+  constructor() {
+    this.userMediaLimit = 0;
+    this.userLibraryLimit = 0;
+    this.movies = [];
+    this.episodes = [];
+    this.songs = [];
+    this.ebooks = [];
+  }
+
+  public setLimits(mediaLimit: number, libraryLimit: number): void {
+    this.userMediaLimit = mediaLimit;
+    this.userLibraryLimit = libraryLimit;
+  }
+
+  public addMedia(user: User, title: string, releaseYear: number, path: string, publicAccess: boolean, metadata: MediaMetadata): void {
+    if (Movie.isMovieMetadata(metadata)) {
+      this.movies.forEach(function(movie) {
+        if (movie.title === title && movie.releaseYear === releaseYear) {
+          throw new Error(`Film znajduje sie juz w bibliotece!`);
+        }
+      });
+      if (!user.isAdmin()) {
+        if (this.countUserLibrary(user) >= this.userLibraryLimit || this.countUserMedia(user, this.movies) >= this.userMediaLimit) {
+          throw new Error(`Przekroczono dopuszczalny limit elementow w bibliotece!`);
+        }
+      }
+      const movie = new Movie(title, releaseYear, path, user.getUsername(), publicAccess, metadata);
+      this.movies.push(movie);
+      console.log(`Dodano film [" ${title} "(" ${releaseYear} ")]\n`);
+    } else if (Episode.isEpisodeMetadata(metadata)) {
+      this.episodes.forEach(function(episode) {
+        if (episode.title === title && episode.releaseYear === releaseYear) {
+          throw new Error(`Odcinek znajduje sie juz w bibliotece!`);
+        }
+      });
+      if (!user.isAdmin()) {
+        if (this.countUserLibrary(user) >= this.userLibraryLimit || this.countUserMedia(user, this.episodes) >= this.userMediaLimit) {
+          throw new Error(`Przekroczono dopuszczalny limit elementow w bibliotece!`);
+        }
+      }
+      const episode = new Episode(title, releaseYear, path, user.getUsername(), publicAccess, metadata);
+      this.episodes.push(episode);
+      console.log(`Dodano odcinek [" ${title} "(" ${releaseYear} ")]\n`);
+    } else if (Song.isSongMetadata(metadata)) {
+      this.songs.forEach(function(song) {
+        if (song.title === title && song.releaseYear === releaseYear) {
+          throw new Error(`Piosenka znajduje sie juz w bibliotece!`);
+        }
+      });
+      if (!user.isAdmin()) {
+        if (this.countUserLibrary(user) >= this.userLibraryLimit || this.countUserMedia(user, this.songs) >= this.userMediaLimit) {
+          throw new Error(`Przekroczono dopuszczalny limit elementow w bibliotece!`);
+        }
+      }
+      const song = new Song(title, releaseYear, path, user.getUsername(), publicAccess, metadata);
+      this.songs.push(song);
+      console.log(`Dodano utwor [" ${title} "(" ${releaseYear} ")]\n`);
+    } else if (Ebook.isEbookMetadata(metadata)) {
+      this.ebooks.forEach(function(ebook) {
+        if (ebook.title === title && ebook.releaseYear === releaseYear) {
+          throw new Error(`Ebook znajduje sie juz w bibliotece!`);
+        }
+      });
+      if (!user.isAdmin()) {
+        if (this.countUserLibrary(user) >= this.userLibraryLimit || this.countUserMedia(user, this.ebooks) >= this.userMediaLimit) {
+          throw new Error(`Przekroczono dopuszczalny limit elementow w bibliotece!`);
+        }
+      }
+      const ebook = new Ebook(title, releaseYear, path, user.getUsername(), publicAccess, metadata);
+      this.ebooks.push(ebook);
+      console.log(`Dodano ebook [" ${title} "(" ${releaseYear} ")]\n`);
+    }
+  }
+
+  countUserMedia<T extends MediaElement>(user: User, mediaContainer: T[]): number {
+    let counter = 0;
+
+    for (const media of mediaContainer) {
+      if (media.getOwner() === user.getUsername()) {
+        counter++;
+      }
+    }
+
+    return counter;
+  }
+
+  public countUserLibrary(user: User): number {
+    return this.countUserMedia(user, this.movies) +
+      this.countUserMedia(user, this.episodes) +
+      this.countUserMedia(user, this.songs) +
+      this.countUserMedia(user, this.ebooks);
+  }
+
+  public clear(): void {
+    this.movies = [];
+    this.episodes = [];
+    this.songs = [];
+    this.ebooks = [];
+  }
+}
